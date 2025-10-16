1b412fe1056a8f9460f70de9c49acd195a9e8982
sh-pt <shanhuang.all@gmail.com>
2025-10-15 20:34:46 -0700

    q1-initial-code

---
+++ src.py
@@ -0,0 +1,50 @@
+import time
+import concurrent.futures
+
+class MyAsyncMapper:
+    def __init__(self, max_workers):
+        self.max_workers = max_workers
+        self.executor = None # save time, only create executor when WITH starts
+
+    def __enter__(self):
+        self.executor = concurrent.futures.ProcessPoolExecutor(max_workers=self.max_workers)
+        return self # return for WITH ... AS ... mapper to assign mapper
+
+    def __exit__(self, exc_type, exc_val, exc_tb): # even not used, need to keep type val and tb
+        if self.executor:
+            self.executor.shutdown(wait=True) # make sure all process finished
+
+    def map(self, func, args):
+        futures = []
+        if type(args) in (list, tuple):
+            for arg in args:
+                futures.append(self.executor.submit(func, *arg))
+        elif type(args) == dict:
+            for kwarg in args.values():
+                futures.append(self.executor.submit(func, **kwarg))
+        else:
+            raise TypeError("Args need to be list, tuple or dict")
+
+        results = []
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                results.append(future.result())
+            except Exception as e:
+                results.append(e)
+        return results
+
+def foo(x, y):
+    time.sleep(1)
+    return (x + y) / x
+
+
+if __name__ == "__main__":
+
+    inputs = [(x, x+1) for x in range(10)]
+
+    with MyAsyncMapper(10) as mapper:
+        start = time.perf_counter()
+        ans = mapper.map(foo, inputs)
+        end = time.perf_counter()
+        print(f'Results: {ans}')
+        print(f'Time taken: {round(end - start,2)} seconds')
